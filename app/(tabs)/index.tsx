7fb9512ee37337c49a0cab20826b7fa13bf76cb6
Joann Carter <jocarter@Joanns-MacBook-Pro.local>
2024-10-09 14:20:47 -0400

    add functionality to read users lat and long and display it

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
 import { Image, Text, View } from "react-native";
+import * as Location from "expo-location";
 
 export default function Index() {
+  const [location, setLocation] = useState({});
+  const [lat, setLat] = useState(Number);
+  const [long,setLong]= useState(Number);
+
+  useEffect(() => { // runs when index component mounts
+    const getPermissions = async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status == 'granted') {
+        console.log('====================================');
+        console.log("Persmissions SUCCESS!", status);
+        let currentLocation = await Location.getCurrentPositionAsync();
+        setLocation(currentLocation);
+        setLat(currentLocation.coords.latitude) ;
+        setLong(currentLocation.coords.longitude);
+        let long = currentLocation.coords.longitude;
+        console.log('====================================');
+      } else{
+        console.log('====================================');
+        console.log("NO LOCATION PERMISSION");
+        console.log('====================================');
+      }
+
+
+      
+    };
+   getPermissions(); //get user permission for location 
+  }, []);
   return (
     <View
       style={{
@@ -9,7 +38,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Image source={require('@/assets/images/DineOrDitchLogo.png')} />
+<Text>latitude: {lat}</Text>
+<Text>longitude: {long}</Text>
 
     </View>
   );
